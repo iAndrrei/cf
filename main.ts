1931b429be3e8b8db3292f4679218f7f2d6449cc
iAndrrei <97289654+iAndrrei@users.noreply.github.com>
2022-02-04 11:33:30 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("")
+    basic.pause(200)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("A+B")
+    basic.pause(200)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B")
+    basic.pause(200)
+    basic.clearScreen()
+})
+basic.forever(function () {
+	
+})
